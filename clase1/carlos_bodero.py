547505e6869194292fea047b71ef88f7c4e7452d
cboderot1 <cboderot1@gmail.com>
2025-06-24 19:51:24 -0500

    deber 1 carlos Bodero

---
+++ clase1/carlos_bodero.py
@@ -0,0 +1,45 @@
+#Ejercicio 1
+def ejercicio1():
+    nombre = input("Nombre: ")
+    edad = input("Edad ")
+    peso = float(input("peso (kg): "))
+    talla = float(input("talla (m): "))
+    imc = peso / (talla*talla)
+    print(f"Hola {nombre} tienes {edad} años y tu imc es {round(imc,2)} estas en")
+    if imc < 18.5:
+        print("Bajo peso")
+    elif imc >= 18.5 and imc <24.9:
+        print("Peso saludable")
+    elif imc >= 25.0 and 29.9:
+        print("sobrepeso")
+    else:
+        print("obesidad")
+
+
+def ejercicio2():
+    Numero_menor = int(input("Ingresa el número menor:"))
+    Numero_mayor = int(input("Ingresa el número mayor:"))
+    if Numero_menor < 0 or Numero_mayor < 0:
+        print("Ingrese Número entero positivo ")
+    elif Numero_menor > Numero_mayor:
+        print()
+    else:
+        numeros = range(Numero_menor,Numero_mayor + 1)
+        cuenta_pares = 0
+        cuenta_numeros = 0
+        for iterador in numeros:
+            print(iterador)
+        print(f"Números pares entre {Numero_menor} y {Numero_mayor} :{cuenta_pares}")
+        print(f"Cantidad total de números en el intervalo: {cuenta_numeros}")
+        
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    ejercicio1()
+    ejercicio2()
+
